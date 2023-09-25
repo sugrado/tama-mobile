529cd6a45c56ad6e2516422eddfe7c48fc493ab8
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2023-09-26 00:04:11 +0300

    refactor: move splash screen configs to bootstrap

--- App.tsx
+++ App.tsx
@@ -2,14 +2,7 @@ import * as React from 'react';
 import 'react-native-gesture-handler';
 import Bootstrap from './src/bootstrap';
 import 'react-native-gesture-handler';
-import SplashScreen from 'react-native-splash-screen';
-import {Platform} from 'react-native';
 
 export default function App() {
-  React.useEffect(() => {
-    if (Platform.OS === 'android') {
-      SplashScreen.hide();
-    }
-  }, []);
   return <Bootstrap />;
 }
--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -3,8 +3,15 @@ import {NavigationContainer} from '@react-navigation/native';
 import Navigation from './routes';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {PaperProvider} from 'react-native-paper';
+import SplashScreen from 'react-native-splash-screen';
+import {Platform} from 'react-native';
 
 export default function Bootstrap() {
+  React.useEffect(() => {
+    if (Platform.OS === 'android') {
+      SplashScreen.hide();
+    }
+  }, []);
   return (
     <SafeAreaProvider>
       <NavigationContainer>
