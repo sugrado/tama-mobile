e205f1280c5ef8fb66e9add712293b9f307aa930
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2023-09-25 23:58:47 +0300

    refactor: move route name constants to constants folder and seperate appointment components

---
+++ src/screens/Appointments/PastAppointments/PastAppointments.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function PastAppointments() {
+  return (
+    <View>
+      <Text>Past Appointments Page</Text>
+    </View>
+  );
+}
