60e8956ca13afc549db9361e912bc1d9223ecbec
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2023-09-25 23:56:44 +0300

    chore: install react-native-paper library

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Navigation from './routes/Navigation';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {PaperProvider} from 'react-native-paper';
 
 export default function Bootstrap() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Navigation />
+        <PaperProvider>
+          <Navigation />
+        </PaperProvider>
       </NavigationContainer>
     </SafeAreaProvider>
   );
