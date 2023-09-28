152a020f419632eb2907cd5e6a5cdbac0d1f8c0a
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2023-09-29 00:02:27 +0300

    refactor: complete login screen

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,81 +1,152 @@
 import React, {useState} from 'react';
-import {Image} from 'react-native';
-import {StyleSheet, View} from 'react-native';
-import {Button, Text} from 'react-native-paper';
-import InputField from '../../components/InputField';
+import {
+  Image,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
+import {DIMENSIONS} from '../../constants';
+import {Button, TextInput, Text} from 'react-native-paper';
 
-const Login = () => {
+function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   return (
     <>
-      <View style={styles.container}>
-        <Image
-          source={require('../../assets/icon.png')}
-          style={styles.loginIcon}
-        />
-        <Text style={styles.headerText}>Giriş Yap</Text>
-        <InputField
-          label="Kullanıcı Adı"
-          value={username}
-          valueChange={input => setUsername(input)}
-          keyboardType="default"
-          style={styles.input}
-        />
-        <InputField
-          label="Parola"
-          style={styles.input}
-          value={password}
-          valueChange={input => setPassword(input)}
-          secureTextEntry={true}
-        />
-        <Button
-          mode="contained"
-          onPress={() => console.log('pressed')}
-          style={styles.loginButton}
-          buttonColor="#4D7E3E"
-          icon="login-variant">
-          Giriş Yap
-        </Button>
-        <Button
-          mode="elevated"
-          onPress={() => console.log('pressed')}
-          style={styles.loginButton}
-          buttonColor="#2196F3"
-          textColor="#fff"
-          icon="calendar-plus">
-          İlk Randevu
-        </Button>
-      </View>
+      <StatusBar backgroundColor="#4D7E3E" />
+      <SafeAreaView style={styles.login_container}>
+        <View style={styles.login_header}>
+          <Image
+            style={styles.login_header_logo}
+            source={require('../../assets/icon_transparent.png')}
+          />
+          <Text style={styles.login_header_text}>
+            <Text style={styles.login_header_text_bold}>{'TAMA - '}</Text>
+            {'Giriş Yap'}
+          </Text>
+        </View>
+        <View style={styles.login_wrapper}>
+          <View style={styles.form}>
+            <TextInput
+              label="Kullanıcı adı"
+              value={username}
+              onChangeText={input => setUsername(input)}
+              theme={{
+                colors: {primary: 'green'},
+              }}
+              style={styles.input}
+            />
+            <TextInput
+              label="Parola"
+              value={password}
+              onChangeText={input => setPassword(input)}
+              theme={{
+                colors: {primary: 'green'},
+              }}
+              secureTextEntry={true}
+              style={styles.input}
+            />
+            <View style={styles.forgot_password}>
+              <TouchableOpacity>
+                <Text style={styles.forgot_password_text}>Şifremi Unuttum</Text>
+              </TouchableOpacity>
+            </View>
+            <Button
+              mode="contained"
+              onPress={() => console.log('pressed')}
+              style={styles.loginButton}
+              buttonColor="#4D7E3E"
+              icon="login-variant">
+              Giriş Yap
+            </Button>
+            <Button
+              mode="elevated"
+              onPress={() => console.log('pressed')}
+              style={styles.firstAppointmentButton}
+              buttonColor="#2b5758"
+              textColor="#fff"
+              icon="calendar-plus">
+              İlk Randevu
+            </Button>
+          </View>
+          <View style={styles.footer}>
+            <Image
+              style={styles.footer_logo}
+              source={require('../../assets/neu_logo.png')}
+            />
+            <Text variant="bodySmall" style={styles.login_footer_text}>
+              Necmettin Erbakan Üniversitesi{'\n'}Tıp Fakültesi Ruh Sağlığı ve
+              Hastalıkları{'\n'}Ana Bilim Dalı
+            </Text>
+          </View>
+        </View>
+      </SafeAreaView>
     </>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 16,
-  },
-  input: {
-    marginVertical: 6,
-    width: '80%', // İsteğe bağlı olarak ekran genişliğine sığdırın
-  },
-  headerText: {
-    fontSize: 24,
+  forgot_password: {width: '100%', alignItems: 'flex-end'},
+  forgot_password_text: {
     fontWeight: 'bold',
-    marginBottom: 24,
+    color: '#4D7E3E',
   },
-  loginButton: {
-    marginTop: 24,
-    width: '60%', // İsteğe bağlı olarak ekran genişliğine sığdırın
+  loginButton: {width: '100%', marginTop: 30},
+  firstAppointmentButton: {width: '100%', marginTop: 30},
+  input: {backgroundColor: '#fff', marginBottom: 10},
+  login_container: {
+    flex: 1,
+    backgroundColor: '#FFF',
   },
-  loginIcon: {
-    width: 100,
-    height: 100,
-    marginBottom: 30,
-    borderRadius: 30,
+  login_header: {
+    alignItems: 'center',
+    paddingTop: 30,
+    paddingBottom: 30,
+    backgroundColor: '#4D7E3E',
+  },
+  login_header_logo: {
+    height: (DIMENSIONS.height * 1.2) / 10,
+    resizeMode: 'contain',
+  },
+  login_header_text: {
+    marginTop: 15,
+    color: '#f0f0f0',
+    fontSize: 16,
+  },
+  login_header_text_bold: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  login_wrapper: {
+    flex: 1,
+    justifyContent: 'space-between',
+    paddingTop: 30,
+    paddingBottom: 15,
+    borderTopRightRadius: 12,
+    borderTopLeftRadius: 12,
+    marginTop: -10,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%',
+    maxWidth: 280,
+  },
+  login_footer_text: {
+    color: '#808080',
+    textAlign: 'center',
+  },
+  footer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  footer_logo: {
+    width: 50,
+    height: 50,
+    marginVertical: 10,
   },
 });
 
