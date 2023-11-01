a033dba907174bed5c59c455fc8e33a805ff62e9
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2023-11-01 13:42:49 +0300

    refactor: redesign select box component

--- src/components/core/SugradoSelectBox.tsx
+++ src/components/core/SugradoSelectBox.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {Text} from 'react-native-paper';
 import SelectDropdown from 'react-native-select-dropdown';
 import Entypo from 'react-native-vector-icons/Entypo';
 
@@ -10,6 +11,10 @@ type SugradoSelectBoxProps = {
   disabled?: boolean;
   disableAutoScroll?: boolean;
   displayValue: (item: SelectBoxData) => string;
+  onBlur?: () => void;
+  style?: any;
+  btnDefaultText?: string;
+  innerRef?: any;
 };
 
 export class SelectBoxData {
@@ -21,55 +26,75 @@ const SugradoSelectBox = ({
   data,
   onSelected,
   label,
+  btnDefaultText = 'Seç...',
   disabled = false,
   disableAutoScroll = true,
   displayValue,
+  onBlur,
+  style,
+  innerRef,
 }: SugradoSelectBoxProps) => {
-  const button_text_style = {color: disabled ? 'gray' : 'black', fontSize: 16};
+  const color = disabled ? 'gray' : 'black';
   return (
-    <SelectDropdown
-      buttonStyle={styles.button_style}
-      buttonTextStyle={button_text_style}
-      rowTextStyle={button_text_style}
-      rowStyle={styles.row_style}
-      dropdownStyle={styles.dropdown_style}
-      selectedRowTextStyle={styles.selected_row_text_style}
-      disabled={disabled}
-      rowTextForSelection={displayValue}
-      buttonTextAfterSelection={displayValue}
-      disableAutoScroll={disableAutoScroll}
-      defaultButtonText={label}
-      dropdownIconPosition="right"
-      renderDropdownIcon={() => (
-        <Entypo
-          name="chevron-down"
-          size={20}
-          style={styles.icon_style}
-          color={disabled ? 'gray' : 'black'}
-        />
-      )}
-      data={data}
-      onSelect={(selectedItem, index) => {
-        onSelected && onSelected(selectedItem, index);
-      }}
-    />
+    <View style={style}>
+      <Text style={styles.label}>{label}</Text>
+      <SelectDropdown
+        ref={innerRef}
+        buttonStyle={styles.button_style}
+        buttonTextStyle={[styles.button_text_style, {color}]}
+        rowTextStyle={styles.rowTextStyle}
+        rowStyle={styles.row_style}
+        dropdownStyle={styles.dropdown_style}
+        selectedRowTextStyle={styles.selected_row_text_style}
+        disabled={disabled}
+        rowTextForSelection={displayValue}
+        buttonTextAfterSelection={displayValue}
+        disableAutoScroll={disableAutoScroll}
+        defaultButtonText={btnDefaultText}
+        dropdownIconPosition="right"
+        renderDropdownIcon={() => (
+          <Entypo
+            name="chevron-down"
+            size={20}
+            style={styles.icon_style}
+            color={disabled ? 'gray' : 'black'}
+          />
+        )}
+        data={data}
+        onSelect={(selectedItem, index) => {
+          onSelected && onSelected(selectedItem, index);
+        }}
+        onBlur={onBlur}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   button_style: {
     width: '100%',
-    borderRadius: 20,
     backgroundColor: 'white',
-    marginBottom: 15,
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: 'gray',
+    height: 56,
+    textAlign: 'left',
+  },
+  rowTextStyle: {
+    fontSize: 16,
+    textAlign: 'left',
+  },
+  button_text_style: {
+    fontSize: 16,
+    color: '#444',
+    textAlign: 'left',
   },
   row_style: {
-    borderRadius: 20,
     backgroundColor: 'white',
     borderColor: 'gray',
   },
   dropdown_style: {
-    borderRadius: 20,
+    borderRadius: 10,
     backgroundColor: 'white',
   },
   selected_row_text_style: {
@@ -77,6 +102,10 @@ const styles = StyleSheet.create({
   },
   icon_style: {
     paddingRight: 10,
+  },
+  label: {
+    marginLeft: 5,
+    marginBottom: 3,
   },
 });
 
