ba5c51e3f24ba2d495b2ca9b8bc2706b5314e706
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2023-12-20 14:07:23 +0300

    refactor: core info card integration to qr code page

--- src/screens/patient/Settings/QRCode.tsx
+++ src/screens/patient/Settings/QRCode.tsx
@@ -1,13 +1,12 @@
 import {Image, StyleSheet, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
-import {Card, Text} from 'react-native-paper';
 import {getQRCode} from '../../../api/patients/patient';
 import Loading from '../../../components/layout/Loading';
-import {COLORS} from '../../../constants';
 import {CustomError, isCritical} from '../../../utils/customErrors';
 import SugradoErrorPage from '../../../components/core/SugradoErrorPage';
 import TopSmallIconLayout from '../../../components/layout/TopSmallIconLayout';
 import SugradoErrorSnackbar from '../../../components/core/SugradoErrorSnackbar';
+import SugradoInfoCard from '../../../components/core/SugradoInfoCard';
 
 const QRCode = () => {
   const [loading, setLoading] = useState<boolean>(false);
@@ -44,25 +43,19 @@ const QRCode = () => {
               pageName="Ayarlar | QR Kodum"
               refreshMethod={loadQRCode}>
               <View style={styles.container}>
+                <SugradoInfoCard
+                  text="Lütfen güvenliğiniz için QR kodunuzu doktorunuz ve
+                  yakınınızdan başkasına göndermeyin ve göstermeyiniz."
+                  icon="warning"
+                  iconSize={25}
+                  style={styles.info_card}
+                />
                 <Image
                   source={{uri: qrCode}}
                   style={styles.qr_code}
                   height={300}
                   width={300}
                 />
-                <Card style={styles.warning_container}>
-                  <Card.Content>
-                    <Text
-                      variant="titleLarge"
-                      style={styles.warning_title_text}>
-                      Uyarı
-                    </Text>
-                    <Text variant="bodyMedium" style={styles.warning_body_text}>
-                      Lütfen güvenliğiniz için QR kodunuzu doktorunuz ve
-                      yakınınızdan başkasına göndermeyin ve göstermeyiniz.
-                    </Text>
-                  </Card.Content>
-                </Card>
               </View>
             </TopSmallIconLayout>
           )}
@@ -74,24 +67,18 @@ const QRCode = () => {
 };
 
 const styles = StyleSheet.create({
-  warning_container: {
-    backgroundColor: COLORS.WARNING,
-    width: '90%',
-  },
-  warning_title_text: {textAlign: 'center'},
-  warning_body_text: {textAlign: 'center', marginTop: 10},
   container: {
     flex: 1,
     alignItems: 'center',
+    paddingVertical: 30,
   },
   qr_code: {
     alignSelf: 'center',
-    marginVertical: 50,
+    marginTop: 20,
   },
-  show_qr_code_text: {
-    textAlign: 'center',
-    color: COLORS.BUTTON_COLOR,
-    fontWeight: 'bold',
+  info_card: {
+    alignSelf: 'center',
+    marginBottom: 10,
   },
 });
 
